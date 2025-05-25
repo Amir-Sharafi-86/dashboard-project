e3bf3b2a0ab8c927f3de1729b63c28462cc3349b
Amir-Sharafi-86 <Amirsharafi8899@gmail.com>
2025-05-25 18:47:55 +0330

    the final style show aside in mobile

--- dashboard-Admin/js/main.js
+++ dashboard-Admin/js/main.js
@@ -1,10 +1,17 @@
-const btnLink  = document.querySelectorAll(".link__sidebar")
-console.log(btnLink);
 
-btnLink.forEach(btn => {
-  btn.addEventListener("click" , (e) => {
-    e.preventDefault() 
-    document.querySelector(".active_link_sidbar").classList.remove("active_link_sidbar")
-    btn.classList.add("active_link_sidbar")
-  })
+
+
+let burger_menu = document.querySelector('.burger_menu')
+let sidbar = document.querySelector('aside')
+let bg_cover = document.querySelector('.bg_cover')
+
+burger_menu.addEventListener('click',e=>{
+    console.log('click')
+    sidbar.classList.add('d_block')
+    bg_cover.classList.add('d_block')
+})
+bg_cover.addEventListener('click',e=>{
+    console.log('click')
+    sidbar.classList.remove('d_block')
+    bg_cover.classList.remove('d_block')
 })
